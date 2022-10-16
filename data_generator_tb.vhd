a9567a2248c32661e44176fdd4d240ef7157605a
henri <95986228+henrimre@users.noreply.github.com>
2022-10-16 18:11:22 +0200

    lash correction

--- data_generator_tb.vhd
+++ data_generator_tb.vhd
@@ -14,20 +14,26 @@ architecture tb of data_generator_tb is
 	
 	signal row_position		: unsigned(10 downto 0) := (others => '0');
 	signal column_position	: unsigned(10 downto 0) := (others => '0');
-	signal data_value			: std_logic_vector (4 downto 0);
+	signal data_value			: std_logic_vector (4 downto 0) := (others => '0');
+	
+	signal test					: std_logic := '0';
 	signal VGA_R				: std_logic_vector(3 downto 0);
-	signal VGA_G				: std_logic_vector(3 downto 0);
-	signal VGA_B				: std_logic_vector(3 downto 0);
+	--signal VGA_G				: std_logic_vector(3 downto 0);
+	--signal VGA_B				: std_logic_vector(3 downto 0);
 begin 
-	UUT : entity work.image_generator port map(
+	UUT : entity work.data_generator port map(
 	clk => clk, 
 	ctrl_area => ctrl_area,	
 	row_position => row_position,	
 	column_position => column_position, 
 	data_value => data_value, 
-	VGA_R => VGA_R, 
-	VGA_G => VGA_G, 
-	VGA_B => VGA_B);
+	test => test,
+	VGA_R => VGA_R
+	--VGA_G => VGA_G, 
+	--VGA_B => VGA_B
+	);
+	
+	data_value <= "00000", "00001" after 10 us;
 	
 	p_stimuli_clk : process
 	begin 
@@ -36,7 +42,7 @@ begin
 		clk <= '1';
 		wait for 20 ns/2;
 	end process p_stimuli_clk;
-		
+
 
 
 end tb;
